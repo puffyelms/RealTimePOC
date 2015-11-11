a0f9a184b893a20c0fcf05bd3bafa7f86008d65f
Tim Getz <timgetz@gmail.com>
2015-11-11 01:43:11 -0500

    Removed double creation of websocket

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -8,8 +8,6 @@ app.factory('WebSocketService', function($q, $rootScope) {
     var callbacks = {};
     // Create a unique callback ID to map requests to responses
     var currentCallbackId = 0;
-    // Create our websocket object with the address to the websocket
-    var ws = new WebSocket("ws://localhost:7001/MarketDataServer/dataSocket");
 
     var attempts = 1;
     var time = 0;
@@ -57,6 +55,50 @@ app.factory('WebSocketService', function($q, $rootScope) {
         cellMonth2: {"currentPrice": 100, "cobPrice": 115},
         cellMonth3: {"currentPrice": 100, "cobPrice": 115},
         cellMonth4: {"currentPrice": 100, "cobPrice": 115}
+    };
+
+    var changed = {};
+    changed['2.5'] = {
+        "coupon": 2.5, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['3.0'] = {
+        "coupon": 3.0, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['3.5'] = {
+        "coupon": 3.5, cellMonth1: {"currentPrice": 101, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['4.0'] = {
+        "coupon": 4.0, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['4.5'] = {
+        "coupon": 4.5, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['5.0'] = {
+        "coupon": 5.0, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
+    };
+    changed['5.5'] = {
+        "coupon": 5.5, cellMonth1: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth2: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth3: {"currentPrice": 99, "cobPrice": 115},
+        cellMonth4: {"currentPrice": 99, "cobPrice": 115}
     };
 
     function createWebSocket() {
